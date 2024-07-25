66bb0939a405b3da2d73073af17ef292fec9220e
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-07-25 11:18:38 +0500

    init

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Title = ({title, extraStyle}) => {
   
   return (
-    <h1 className={`text-green leading-[64px] font-bold text-[64px] ${extraStyle}`}>{title}</h1>
+    <h1 className={`text-green capitalize leading-[64px] font-bold text-[64px] ${extraStyle}`}>{title}</h1>
   )
 }
 
